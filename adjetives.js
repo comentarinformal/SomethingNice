0cb16e14aa21fae0e20f8bec082ed5e14be0d54a
Francisco Bento <franciscobdsjunior@gmail.com>
2018-10-03 22:23:00 -0300

    Add nice words

--- adjetives.js
+++ adjetives.js
@@ -1,6 +1,18 @@
 // Wow, this is a [____] dog!
 // That's such a [____] gesture, thank you!
 
-var adjetives = [
+var adjectives = [
   'nice',
+  'pretty',
+  'good',
+  'cool',
+  'lovely',
+  'gorgeous',
+  'graceful',
+  'handsome',
+  'sweet',
+  'splendid',
+  'enchanting',
+  'kind',
+  'gracious',
 ];
